f906a78e2f75329463b3ed8e5333e1360676e930
jkmthiago <thiago-eleuterio@hotmail.com>
2024-09-16 21:24:39 +0000

    s

--- api/src/config/config.go
+++ api/src/config/config.go
@@ -1,26 +1,23 @@
 package config
 
 import (
-	"log"
 	"os"
-	"strconv"
 
-	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 var (
 	Db_user     = ""
 	Db_host     = ""
-	Db_port     = 0
+	Db_port     = ""
 	Db_password = ""
 	Db_name     = ""
-	Api_port    = 0
+	Api_port    = ""
 )
 
 // Carrega as variáveis de ambiente
 func LoadEnvVar() {
-	Api_port= os.Getenv("API_PORT")
+	Api_port = os.Getenv("API_PORT")
 	Db_port = os.Getenv("DB-PORT")
 	Db_user = os.Getenv("DB_USER")
 	Db_host = os.Getenv("DB_HOST")
